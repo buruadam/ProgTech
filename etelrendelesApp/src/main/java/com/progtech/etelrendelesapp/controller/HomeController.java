eac507622042d7fbcf866e5e30d1a739a475fb00
Juhász Gergő <123juhasz.gergo123@gmail.com>
2024-05-15 13:40:58 +0200

    Kijelentkezés megvalósítása

--- etelrendelesApp/src/main/java/com/progtech/etelrendelesapp/controller/HomeController.java
+++ etelrendelesApp/src/main/java/com/progtech/etelrendelesapp/controller/HomeController.java
@@ -1,4 +1,34 @@
 package com.progtech.etelrendelesapp.controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.event.ActionEvent;
+import java.io.IOException;
+
 public class HomeController {
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    public void handleLogout(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/progtech/etelrendelesapp/view/login-view.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = new Stage();
+            stage.setTitle("Login");
+            stage.setScene(new Scene(root));
+            stage.show();
+
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            currentStage.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
